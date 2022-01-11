6ff554ff55bb4d09f6715d8970326ecafe8f5abf
BlesMateo <blesildaangelicamateo@gmail.com>
2022-01-10 19:08:51 -0500

    Practiced installing and initializing RSpec within a Rails application by setting up the Jungle app for testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20220107004235) do
+ActiveRecord::Schema.define(version: 20220108145615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
