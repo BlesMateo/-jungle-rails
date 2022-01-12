888e6e50a1f254759fc0806989cde1224b73ed1f
BlesMateo <blesildaangelicamateo@gmail.com>
2022-01-11 21:14:22 -0500

    Created a nested description within the Product description and define examples for each validation in the Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,4 +7,33 @@ RSpec.describe Product, type: :model do
       @product = Product.new(id: 1, name: "Grey Wool Toque", price_cents: 200, quantity: 75, category: @category)
 end
 
-it "is "
+it "is valid with valid attributes" do
+  expect(@product).to be_valid
+end
+
+it "should have a valid product name" do
+  @product.name = nil
+  @product.valid?
+  expect(@product.errors.full_messages).to_not be_empty
+end
+
+it "should have a valid price" do
+  @product.price_cents = nil
+  @product.valid?
+  expect(@product.errors.full_messages).to_not be_empty
+end
+
+it "should have a valid quantity" do
+  @product.quantity = nil
+  @product.valid?
+  expect(@product.errors.full_messages).to_not be_empty
+end
+
+it "should have a valid product category" do
+  @product.category = nil
+  @product.valid?
+  expect(@product.errors.full_messages).to_not be_empty
+end
+
+end
+end
